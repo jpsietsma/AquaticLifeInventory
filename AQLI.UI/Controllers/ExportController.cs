19ca6e2f34cdb41e3abe2217c6cde7479f7d5558
jpsietsma <jpsietsma@gmail.com>
2021-01-17 02:16:36 -0500

    Exporting Scanned receipt, and exporting/generating CSV of purchases are both working.

--- AQLI.UI/Controllers/ExportController.cs
+++ AQLI.UI/Controllers/ExportController.cs
@@ -21,111 +21,114 @@ namespace AQLI.UI.Controllers
             DataSource = _factory;
         }
 
-        public async Task<FileContentResult> ExportPurchaseInvoice(string fileType, int purchaseID)
-        {
-            switch (fileType)
+        #region Purchase Invoice Export Methods...
+
+            public async Task<FileContentResult> ExportPurchaseInvoice(int purchaseID, string fileType = "pdf")
             {
-                case "csv":
+                var invoice = DataSource.List_PurchaseInvoices().Where(I => I.PurchaseInvoiceID == purchaseID).First();
+                string filename = invoice.InvoiceFilePath.Split('/').Last().Split('.').First() + ".csv";
+
+                switch (fileType)
+                {
+                    case "csv":
                     {
-                        return ExportAsExcel(purchaseID);                        
+                        //Return CSV file of purchases associated with the invoice.  Headers included.
+                        return await BuildCSV(invoice.Purchases, true, filename, true, new string[] { "Store", "Tank", "Invoice", "PurchaseCategory" });                        
                     }
-                case "pdf":
+                    case "aqlpdf":
                     {
-                        return await ExportAsPDF(purchaseID);
-                    }
-                default:
-                    {
+                        //Prompt generation and download of PDF version of AQL Invoice details window
+                        
+                        //      ...determine code to generate PDF here....
                         return null;
                     }
-            }
-        }
-
-        private FileContentResult ExportAsExcel(int purchaseID)
-        {
-            var list = Database.PurchaseInvoices.Where(I => I.PurchaseInvoiceID == purchaseID).First().Purchases;
-            
-            //var excludedColumns = new string[] { "ContactId", "Title", "Suffix", "MiddleName", "EmailAddress", "Active", "StreetAddress2", "FarmAssociation" };
-            //return BuildCSV(list.ListRecipients, true, list.ListName, true, excludedColumns);
-
-            return null;
-        }
-
-        private async Task<FileContentResult> ExportAsPDF(int purchaseID)
-        {
-            string invoicePath = Database.PurchaseInvoices.Where(I => I.PurchaseInvoiceID == purchaseID).First().InvoiceFilePath;
-
-            var bytes = await System.IO.File.ReadAllBytesAsync(invoicePath);
-
-            return File(bytes, "application/pdf");
-        }
-
-        /// <summary>
-        /// Build a CSV string of a list of any class of objects, with option to directly prompt user to download.
-        /// </summary>
-        /// <typeparam name="T">Class of the view models contained by the passed in list</typeparam>
-        /// <param name="dataList">List of view models</param>
-        /// <param name="promptDownload">prompt user to download file?</param>
-        /// <param name="promptFilename">Filename to be defaulted in download prompt</param>
-        /// <param name="includeHeader">Write headers to the csv file?</param>
-        /// <param name="excludedColumns">string array of properties to be excluded</param>
-        /// <returns>Returns csv string of list if promptDownload is set to false, otherwise returns FileContentResult which results in download of csv</returns>
-        public dynamic BuildCSV<T>(List<T> dataList, bool promptDownload = false, string promptFilename = null, bool includeHeader = false, params string[] excludedColumns) where T : class
-        {
-            StringBuilder csvData = new StringBuilder();
-
-            Type viewModelClass = typeof(T);
-
-            if (includeHeader)
-            {
-                StringBuilder header = new StringBuilder();
-                var vmProperties = viewModelClass.GetProperties();
-
-                //Exclude headers provided by user in excludedParameters string[]
-                //If no headers specifically excluded, use all property names as headers from view model class
-                foreach (var property in vmProperties)
-                {
-                    if (!excludedColumns.Contains(property.Name))
+                    default:
                     {
-                        header.Append($@"{property.Name}, ");
+                        //Prompt download of scanned receipt file in whatever format it was uploaded in
+                        var bytes = await System.IO.File.ReadAllBytesAsync(invoice.InvoiceFilePath);
+
+                        return File(bytes, "application/octet-stream", filename);
+                    }                    
+                }
+            }
+
+            /// <summary>
+            /// Build a CSV string of a list of any class of objects, with option to directly prompt user to download.
+            /// </summary>
+            /// <typeparam name="T">Class of the view models contained by the passed in list</typeparam>
+            /// <param name="dataList">List of view models</param>
+            /// <param name="promptDownload">prompt user to download file?</param>
+            /// <param name="promptFilename">Filename to be defaulted in download prompt</param>
+            /// <param name="includeHeader">Write headers to the csv file?</param>
+            /// <param name="excludedColumns">string array of properties to be excluded</param>
+            /// <returns>Returns csv string of list if promptDownload is set to false, otherwise returns FileContentResult which results in download of csv</returns>
+            private async Task<dynamic> BuildCSV<T>(List<T> dataList, bool promptDownload = false, string promptFilename = null, bool includeHeader = false, params string[] excludedColumns) where T : class
+            {
+                StringBuilder csvData = new StringBuilder();
+
+                Type viewModelClass = typeof(T);
+
+                if (includeHeader)
+                {
+                    StringBuilder header = new StringBuilder();
+                    var vmProperties = viewModelClass.GetProperties();
+
+                    //Exclude headers provided by user in excludedParameters string[]
+                    //If no headers specifically excluded, use all property names as headers from view model class
+                    foreach (var property in vmProperties)
+                    {
+                        if (!excludedColumns.Contains(property.Name))
+                        {
+                            header.Append($@"{property.Name}, ");
+                        }
                     }
+
+                    csvData.AppendLine(header.ToString());
                 }
 
-                csvData.AppendLine(header.ToString());
-            }
-
-            //Build each item row and convert to csv, then append to csvData stringbuilder
-            foreach (var item in dataList)
-            {
-                StringBuilder itemRowData = new StringBuilder();
-
-                var itemProperties = item.GetType().GetProperties();
-                foreach (var property in itemProperties)
+                //Build each item row and convert to csv, then append to csvData stringbuilder
+                foreach (var item in dataList)
                 {
-                    if (!excludedColumns.Contains(property.Name))
+                    StringBuilder itemRowData = new StringBuilder();
+
+                    var itemProperties = item.GetType().GetProperties();
+                    foreach (var property in itemProperties)
                     {
-                        itemRowData.Append(property.GetValue(item) + ", ");
+                        if (!excludedColumns.Contains(property.Name))
+                        {
+                            if (property.Name == "Cost" || property.Name == "ExtCost")
+                            {
+                                var formattedCurrency = ((decimal)(property.GetValue(item))).ToString("C");
+
+                                itemRowData.Append(formattedCurrency + ", ");
+                            }
+                            else
+                            {
+                                itemRowData.Append(property.GetValue(item) + ", ");
+                            }                            
+                        }
                     }
+
+                    //Add our row data to the overall csvData, then clear our itemRowData object for next iteration
+                    csvData.AppendLine(itemRowData.ToString());
+                    itemRowData.Clear();
                 }
 
-                //Add our row data to the overall csvData, then clear our itemRowData object for next iteration
-                csvData.AppendLine(itemRowData.ToString());
-                itemRowData.Clear();
+                if (promptDownload)
+                {
+                    //if prompt filename is null, 
+                    //   set it to "MailingListDownload.csv" 
+                    //otherwise check if prompt filename contains file extension, 
+                    //    if no: add it and then set as the filename
+                    //    if yes: set the provided name as the filename
+                    promptFilename = promptFilename == null ? "WITExportedCSV.csv" : promptFilename.Contains(".csv") ? promptFilename : promptFilename + ".csv";
+
+                    return File(new UTF8Encoding().GetBytes(csvData.ToString()), "text/csv", promptFilename);
+                }
+
+                return csvData.ToString();
             }
 
-            if (promptDownload)
-            {
-                //if prompt filename is null, 
-                //   set it to "MailingListDownload.csv" 
-                //otherwise check if prompt filename contains file extension, 
-                //    if no: add it and then set as the filename
-                //    if yes: set the provided name as the filename
-                promptFilename = promptFilename == null ? "WITExportedCSV.csv" : promptFilename.Contains(".csv") ? promptFilename : promptFilename + ".csv";
-
-                return File(new UTF8Encoding().GetBytes(csvData.ToString()), "text/csv", promptFilename);
-            }
-
-            return csvData.ToString();
-        }
-
+        #endregion
     }
 }
